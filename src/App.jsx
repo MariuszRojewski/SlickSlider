ce1d7a27a92a54e4892c0dbc932b2da54240e94b
Mariusz Rojewski <rojewski@webkon.eu>
2023-08-18 12:35:27 +0200

    08 SlicSlider

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carusel from './Carusel';
+import SlickCarusel from './SlickCarusel';
+
+const App = () => {
+  return (
+    <main>
+      <SlickCarusel />
+    </main>
+  );
+};
+export default App;
